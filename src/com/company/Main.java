04ce2f299bcf3cc48975a35b7eac2863c52a8afb
Jana VanMed <jana.vanmed@gmail.com>
2022-03-23 15:22:35 +0200

    Innitial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Assignment Program that prints out 25% of whatever value is given
+
+    int Value = 1000;
+    double Discount = Value * 0.25;
+    System.out.println("Discount is 25% of Value:" + Discount);
+
+    }
+}
